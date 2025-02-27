0766197604ee6b0a3a81343553b7a2b9efaa739c
Sunil Prajapat <22bcs11871@cuchd.in>
2025-02-27 23:38:22 +0530

    Update EmployeeManagement.java

--- EmployeeManagement.java
+++ EmployeeManagement.java
@@ -1,4 +1,5 @@
-import java.io.*;
+// 2. Create a menu-based Java application with the following options. 
+// 1.Add an Employee 2. Display All 3. Exit If option 1 is selected, the application should gather details of the employee like employee name, employee id, designation and salary and store it in a file. If option 2 is selected, the application should display all the employee details. If option 3 is selected the application should exit.import java.io.*;
 import java.util.*;
 
 class Employee implements Serializable {
